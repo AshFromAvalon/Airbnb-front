da06e15735ff08dd0092334ff4f7f964024ec7c2
Thomas Le Bihan <thomas.66days@gmail.com>
2021-05-21 10:11:24 +0200

    handle permission refused

--- containers/MapScreen.js
+++ containers/MapScreen.js
@@ -7,6 +7,7 @@ import {
   Dimensions,
   SafeAreaView,
   Platform,
+  StatusBar,
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
@@ -43,59 +44,64 @@ const MapScreen = () => {
           console.log(error);
         }
       } else {
-        setError(true);
+        try {
+          const res = await axios.get(
+            "https://express-airbnb-api.herokuapp.com/rooms/around"
+          );
+          setFlats(res.data);
+        } catch (error) {
+          console.log(error);
+        }
       }
       setIsLoading(false);
     };
     askPermission();
   }, []);
 
+  const getInitialRegion = () => {
+    return {
+      latitude: userCoords ? userCoords.latitude : 48.8534,
+      longitude: userCoords ? userCoords.longitude : 2.3488,
+      latitudeDelta: 0.4,
+      longitudeDelta: 0.4,
+    };
+  };
+
   const { map, container, marker, markerText, markerContainer } = styles;
 
   return !isLoading ? (
-    error ? (
-      <SafeAreaView style={container}>
-        <Text>Permisson refusée</Text>
-      </SafeAreaView>
-    ) : (
-      <View style={container}>
-        <MapView
-          style={map}
-          provider={PROVIDER_GOOGLE}
-          showsUserLocation={true}
-          followsUserLocation={true}
-          showsMyLocationButton={true}
-          initialRegion={{
-            latitude: userCoords.latitude,
-            longitude: userCoords.longitude,
-            latitudeDelta: 0.4,
-            longitudeDelta: 0.4,
-          }}
-        >
-          {flats.map((flat, index) => {
-            return (
-              <Marker
-                key={index}
-                onPress={() => navigation.navigate("Flat", { id: flat._id })}
-                coordinate={{
-                  latitude: flat.location[1],
-                  longitude: flat.location[0],
-                }}
-              >
-                <View style={markerContainer}>
-                  <View style={marker}>
-                    <Text style={markerText}>{flat.price} €</Text>
-                  </View>
-                  <View style={{ marginTop: -7 }}>
-                    <Octicons name="triangle-down" size={20} color="black" />
-                  </View>
+    <View style={container}>
+      <MapView
+        style={map}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation={true}
+        followsUserLocation={true}
+        showsMyLocationButton={true}
+        initialRegion={getInitialRegion()}
+      >
+        {flats.map((flat, index) => {
+          return (
+            <Marker
+              key={index}
+              onPress={() => navigation.navigate("Flat", { id: flat._id })}
+              coordinate={{
+                latitude: flat.location[1],
+                longitude: flat.location[0],
+              }}
+            >
+              <View style={markerContainer}>
+                <View style={marker}>
+                  <Text style={markerText}>{flat.price} €</Text>
                 </View>
-              </Marker>
-            );
-          })}
-        </MapView>
-      </View>
-    )
+                <View style={{ marginTop: -7 }}>
+                  <Octicons name="triangle-down" size={20} color="black" />
+                </View>
+              </View>
+            </Marker>
+          );
+        })}
+      </MapView>
+    </View>
   ) : (
     <LoadingActivity style={map} />
   );
@@ -109,7 +115,7 @@ const styles = StyleSheet.create({
     width: Dimensions.get("window").width,
     height:
       Platform.OS === "android"
-        ? Dimensions.get("window").height - 100
+        ? Dimensions.get("window").height
         : Dimensions.get("window").height - 170,
   },
 
